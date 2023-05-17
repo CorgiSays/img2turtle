e0cc9c8ded82aac7800d1e223e530a5204363875
CorgiSays <132166223+CorgiSays@users.noreply.github.com>
2023-05-17 10:14:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 imp = []
 
 # use any image to desmos program which returns output in the form of an equation like (x or y) = value\left\{value\le y \value\right\}, run the program then paste it in
@@ -20,17 +21,32 @@ while True:
     else:
         te = []
         break
-
-turtle.screensize(1000,1000,bg=None)
+turtle.colormode(255)
+turtle.screensize(400,400,bg=None)
 turtle.speed(0)
-for e in imp:
+turtle.tracer(0,0)
+for e in range(0,len(imp)-1,1):
     turtle.up()
-    if e[3] == "y":
-        turtle.goto(float(e[1]),float(e[0]))
+    #uncomment this for rainbow colour
+    #turtle.pencolor(int(randint(0, 255)),int(randint(0, 255)),int(randint(0, 255)))
+    turtle.pencolor("orange")
+    if e == len(imp)-1:
+        break
+    if imp[e][3] == "y":
+        turtle.up()
+        turtle.goto(float(imp[e][1]),float(imp[e][0]))
         turtle.down()
-        turtle.goto(float(e[2]),float(e[0]))
+        turtle.goto(float(imp[e][2]),float(imp[e][0]))
+        turtle.up()
     else:
-        turtle.goto(float(e[0]),float(e[1]))
+        turtle.up()
+        turtle.goto(float(imp[e][0]),float(imp[e][1]))
         turtle.down()
-        turtle.goto(float(e[0]),float(e[2]))
+        turtle.goto(float(imp[e][0]),float(imp[e][2]))
+        turtle.up()
+    if randint(0,3) == 1:
+        turtle.update() # make go fast by commenting this and the one above
+turtle.update() 
+turtle.hideturtle()
+turtle.delay(999999999)
 input()
